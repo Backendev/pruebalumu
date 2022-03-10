6d32d07aa6d37e3adc7ffed7db1d785dafa59e42
Backendev <67878764+Backendev@users.noreply.github.com>
2022-03-10 14:15:56 -0500

    Delete singleton.py

--- singleton.py
+++
@@ -1,8 +0,0 @@
-
-class Singleton(type):
-    _instances = {}
-    def __call__(cls):
-        if cls not in cls._instances:
-            instance = super().__call__()
-            cls._instances[cls] = instance
-        return cls._instances[cls]
